9800d846b0b65688ac4771e995f930592301f3a0
Monique Dias <moniquedbandas@gmail.com>
2025-06-12 01:05:55 -0300

    Projeto em andamento

---
+++ src/controllers/orders-controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+import { knex } from "@/database/knex";
+import z from "zod";
+
+class OrdersController {
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        table_session_id: z.number(),
+        product_id: z.number(),
+        quantity: z.number(),
+      });
+      const { table_session_id, product_id, quantity } = bodySchema.parse(
+        request.body
+      );
+
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        .where({ id: table_session_id })
+        .first();
+      if (!session) {
+        throw new AppError("session table not found");
+      }
+      if (session.closed_at) {
+        throw new AppError("this table is closed");
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first();
+      if (!product) {
+        throw new AppError("product not found");
+      }
+      return response.status(201).json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { OrdersController };
