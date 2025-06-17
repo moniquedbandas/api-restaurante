9ebec9592c03a32490c30fd42f2e15fb375c0935
Monique Dias <moniquedbandas@gmail.com>
2025-06-17 17:22:58 -0300

    Projeto de API de um restaurante finalizado.

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -6,5 +6,10 @@ const ordersRoutes = Router();
 const ordersController = new OrdersController();
 
 ordersRoutes.post("/", ordersController.create);
+ordersRoutes.get("/table-session/:table_session_id", ordersController.index);
+ordersRoutes.get(
+  "/table-session/:table_session_id/total",
+  ordersController.show
+);
 
 export { ordersRoutes };
